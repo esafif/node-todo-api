8d5bd7834acfb5b76aa30d958a288b176855f8a9
esafif <esafif637@gmail.com>
2017-12-09 23:05:42 +0700

    Validators, Type, Default

--- server/server.js
+++ server/server.js
@@ -5,28 +5,55 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minLenght: 1,
+        trim: true
     },
     completed:{
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt:{
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-var newTodo = new Todo({
-    text: 'Cook dinner',
+// var newTodo = new Todo({
+//     text: 'Cook dinner',
+// });
+
+// var otherTodo = new Todo({
+//     // text: 'Feed the cat',
+//     // completed: true,
+//     // completedAt: 123
+//     text: 'Cook dinner 22',
+// })
+
+// otherTodo.save().then((doc) =>{
+//     console.log(JSON.stringify( doc, undefined, 2));
+// }, (e) => {
+//     console.log('unable to save data', e);
+// });
+
+
+//User
+var User = mongoose.model('User', {
+    Email: {
+        type: String,
+        required: true,
+        trim: true,
+        minLenght: 1
+    }
 });
 
-var otherTodo = new Todo({
-    text: 'Feed the cat',
-    completed: true,
-    completedAt: 123
-})
+var user = new User({
+    Email: 'esafif637@gmail.com'
+});
 
-otherTodo.save().then((doc) =>{
-    console.log('Saved todo ', doc);
+user.save().then((doc) =>{
+    console.log('User saved ', doc);
 }, (e) => {
-    console.log('unable to save data');
+    console.log('Unable to save user', e);
 });
