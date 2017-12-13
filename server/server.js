611f68d9d8574e73be7667dc44ad0d0026f6b642
esafif <esafif637@gmail.com>
2017-12-13 22:37:07 +0700

    method post

--- server/server.js
+++ server/server.js
@@ -5,3 +5,25 @@ var  {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) =>{
+    //console.log(req.body);
+    var todo = new Todo({
+        text : req.body.text,
+        completed : false,
+        completedAt: null
+    });
+
+    todo.save().then((doc) =>{
+        res.send(doc);
+    }, (e) =>{
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () =>{
+    console.log("run on localhost");
+});
