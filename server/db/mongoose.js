a175c05e105af5c2133012bd178ac2d7ceaa5f76
esafif <esafif637@gmail.com>
2017-12-14 19:32:38 +0700

    DB URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb:esafif=:safif3110@ds137826.mlab.com:37826/node-todo-api' ||'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
