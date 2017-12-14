2cbbfbad77cad78c1b7e29582e6e5c38b3201d70
esafif <esafif637@gmail.com>
2017-12-14 17:52:25 +0700

    add test GET/todos

--- server/server.js
+++ server/server.js
@@ -37,16 +37,16 @@ app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
 
     if(!ObjectId.isValid(id)){
-        return res.status(403).send();
+        return res.status(404).send();
     }
 
     Todo.findById(id).then((todo) => {
         if(!todo){
-            return res.status(403).send();
+            return res.status(404).send();
         }
         res.send({todo});
     }).catch((e) => {
-        res.status(403).send();
+        res.status(404).send();
     });
 });
 
