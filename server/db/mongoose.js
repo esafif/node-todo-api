868226824e199fbffebda5cb7ebd477caf75c966
esafif <esafif637@gmail.com>
2017-12-15 22:39:37 +0700

    adding config.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp' ||  'mongodb://esafif:safif3110@ds137826.mlab.com:37826/node-todo-api');
+mongoose.connect('mongodb://esafif:safif3110@ds137826.mlab.com:37826/node-todo-api');
 
 module.exports = {
     mongoose
 };
+
 
 //'mongodb://localhost:27017/TodoApp' || 
