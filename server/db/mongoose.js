fb3394056ba13028e18fa6cc3079ad08f9cb53be
esafif <esafif637@gmail.com>
2017-12-18 04:01:22 +0700

    Adding test db in config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://esafif:safif3110@ds137826.mlab.com:37826/node-todo-api');
+mongoose.connect('mongodb://localhost:27017/TodoApp'||'mongodb://esafif:safif3110@ds137826.mlab.com:37826/node-todo-api');
 
 module.exports = {
     mongoose
